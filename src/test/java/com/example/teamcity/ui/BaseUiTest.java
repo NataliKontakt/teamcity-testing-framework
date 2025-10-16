9506b72ab5962ae564c2297ad1ea7f6fbb207cdf
natali-kontakt <natalikontaktgood@gmail.com>
2025-10-16 20:21:06 +0500

    add LoginPage

--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -1,51 +1,29 @@
 package com.example.teamcity.ui;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.example.teamcity.BaseTest;
-import org.testng.annotations.Test;
+import com.example.teamcity.api.config.Config;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
 
-import static io.qameta.allure.Allure.step;
-@Test(groups = {"Regression"})
+import java.util.Map;
+
 public class BaseUiTest extends BaseTest {
-    @Test(description = "User should be able to create project", groups = {"Positive"})
-    public void userCreatesProject(){
-        // подготовка окружения
-        step("Login as user" );
+    @BeforeSuite(alwaysRun = true)
+    public void setupUiTest() {
+        Configuration.browser = Config.getProperty("browser");
+        Configuration.baseUrl = "http://" + Config.getProperty("host");
+        // НЕТ ПИШИТЕ UI ТЕСТЫ С ЛОКАЛЬНЫМ БРАУЗЕРОМ
+        // А ПОТОМ ЗАПУСКАЕТЕ НА REMOTE BROWSER
+        Configuration.remote = Config.getProperty("remote");
+        Configuration.browserSize = Config.getProperty("browserSize");
 
-        // взаимодействие с UI
-        step("Open create project page (http://localhost:8111/admin/createObjectMenu.html)" );
-        step("Send all project parameters (repository url)" );
-        step("Click Proceed" );
-        step("Fix Project Name and Build Type name values" );
-        step("Click Proceed" );
-
-        // проверка состояния API
-        //(корректность отправки данных с UI на API)
-        step("Check that all entities (project, build type) was successfuly with correct data on API level " );
-        // проверка состояния UI
-        //(корректность считывания данных и отображения данных на UI)
-        step("Check that project is visible on Projects Page (http://localhost:8111/favorite/projects)" );
-
+        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of("enableVNC", true, "enableLog", true));
     }
-    @Test(description = "User should not be able to create project without wame", groups = {"Negative"})
-    public void userCreatesProjectWithoutName(){
-        // подготовка окружения
-        step("Login as user" );
-        step("Check number of projects");
 
-        // взаимодействие с UI
-        step("Open create project page (http://localhost:8111/admin/createObjectMenu.html)" );
-        step("Send all project parameters (repository url)" );
-        step("Click Proceed" );
-        step("Set Project name value is empty" );
-        step("Click Proceed" );
-        // проверка состояния API
-        //(корректность отправки данных с UI на API)
-        step("Check that number of projects did not change" );
-
-
-        // проверка состояния UI
-        //(корректность считывания данных и отображения данных на UI)
-        step("Check that error appears `Project name must not be empty`" );
-
+    @AfterMethod(alwaysRun = true)
+    public void closeWebDriver() {
+        Selenide.closeWebDriver();
     }
 }
